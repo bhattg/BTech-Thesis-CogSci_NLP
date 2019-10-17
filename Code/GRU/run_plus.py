679173595ec653b031cfb01709f988fafb2fc902
bhattg <iitdgantavya@gmail.com>
2019-10-17 19:14:16 +0530

    not important

--- Code/GRU/run_plus.py
+++
@@ -1,5 +0,0 @@
-from agreement_acceptor import GramHalfPlusSentence
-import filenames
-
-pvn = GramHalfPlusSentence(filenames.deps, prop_train=0.1, output_filename='output_log.txt', len_after_verb=10)
-pvn.pipeline(train=True, load=True, epochs=10, model_prefix='lstm_ghp_plus10',test_size=50000, data_name='plus10', model="lstm_ghp_plus10.pkl")
