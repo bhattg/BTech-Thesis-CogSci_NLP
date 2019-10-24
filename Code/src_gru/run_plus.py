1a7a4531ba4836e5a59543583b0cf42e9e1dd381
bhattg <iitdgantavya@gmail.com>
2019-10-24 15:04:37 +0530

    major re organization of code folder

--- Code/src_gru/run_plus.py
+++ Code/src_gru/run_plus.py
@@ -1,5 +1,5 @@
 from agreement_acceptor import FullGramSentence
 import filenames
 
-pvn = FullGramSentence(filenames.deps, prop_train=0.1, output_filename='output_log.txt', len_after_verb=10)
+pvn = FullGramSentence(filenames.deps, prop_train=0.1, output_filename='output_log.txt', len_after_verb=10, embedding_size=50, hidden_dim = 50)
 pvn.pipeline(train=False,load=True,model="gru_fullGram.pkl", load_data=True, epochs=10, model_prefix='gru_fullGram',test_size=0, data_name='fullGram')
--- Code/src_lstm_100/run_plus.py
+++ Code/src_lstm_100/run_plus.py
@@ -1,5 +1,5 @@
 from agreement_acceptor import FullGramSentence
 import filenames
 
-pvn = FullGramSentence(filenames.deps, prop_train=0.8,hidden_dim=100,embedding_size=100, output_filename='output_log.txt', len_after_verb=10)
+pvn = FullGramSentence(filenames.deps, prop_train=0.8,hidden_dim=100,embedding_size=100, output_filename='output_log.txt', len_after_verb=10,embedding_size=50, hidden_dim = 50)
 pvn.pipeline(train=True,load=False,model="lstm_fullGram_100.pkl", load_data=True, epochs=10, model_prefix='lstm_fullGram_100',test_size=7000, data_name='fullGram')
